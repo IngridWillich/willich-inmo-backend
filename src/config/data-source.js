28349908b01ea8de474e1d140d91b31a53c470bd
IngridWillich <ingridwillich@gmail.com>
2025-06-04 01:15:21 -0300

    Configuración para Render

--- src/config/data-source.js
+++ src/config/data-source.js
@@ -1,23 +1,43 @@
+// import "reflect-metadata";
+// import { DataSource } from "typeorm";
+// import { Property } from "../entities/Property";7
+
+
+// export const AppDataSource = new DataSource({
+//     type: "postgres",
+//     host: process.env.DB_HOST || "localhost",
+//     port: Number(process.env.DB_PORT) || 5432,
+//     username: process.env.DB_USER || "postgres",
+//     password: process.env.DB_PASSWORD ||"mentagringa",
+//     database: process.env.DB_NAME || "inmo_db",
+//     synchronize: false, 
+//     logging: true,
+//     entities: [Property],
+//     migrations: ["src/migrations/**/*.ts"],
+//     subscribers: ["src/subscribers/**/*.ts"],
+// });
+
+
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { Property } from "../entities/Property";7
-
+import { Property } from "../entities/Property";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 5432,
-    username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD ||"mentagringa",
-    database: process.env.DB_NAME || "inmo_db",
-    synchronize: false, 
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: true,
+    extra: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    },
+    synchronize: false,
     logging: true,
     entities: [Property],
-    migrations: ["src/migrations/**/*.ts"],
-    subscribers: ["src/subscribers/**/*.ts"],
+    migrations: ["dist/migrations/**/*.js"],
+    subscribers: ["dist/subscribers/**/*.js"],
 });
-
-
-// export const UserModel= AppDataSource.getRepository(User)
-// export const CredentialsModel= AppDataSource.getRepository(Credentials)
-// export const AppointmentModel=  AppDataSource.getRepository(Appointment)
